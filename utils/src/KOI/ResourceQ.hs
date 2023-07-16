bade830f1634dcf9a16905dc701ec370cdd10ca8
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2023-07-16 11:52:40 -0700

    Add ResourceQ

---
+++ utils/src/KOI/ResourceQ.hs
@@ -0,0 +1,57 @@
+-- | A deck and a discard pile.
+module KOI.ResourceQ
+  ( ResourceQ
+  , rqEmpty
+  , rqFromListRandom
+  , rqFromListOrdered
+  , rqTake
+  , rqDiscard
+  ) where
+
+import KOI.RNGM
+
+-- | A deck and a discard pile.  If the deck runs out, then we
+-- shuffle the discard pile.
+data ResourceQ a = ResourceQ
+  { qAvailable   :: [a]
+  , qDiscarded   :: [a]
+  , qRandom      :: RNG
+  }
+
+-- | Empty deck and discard.
+rqEmpty :: Gen (ResourceQ a)
+rqEmpty =
+  do qRandom <- splitRNG
+     return ResourceQ { qAvailable = [], qDiscarded = [], .. }
+
+-- | Empty deck, but the given things are in the discard.
+rqFromListRandom :: [a] -> Gen (ResourceQ a)
+rqFromListRandom qDiscarded =
+  do qRandom <- splitRNG
+     return ResourceQ { qAvailable = [], .. }
+
+-- | Empty discard, set the deck to the given list.
+rqFromListOrdered :: [a] -> Gen (ResourceQ a)
+rqFromListOrdered qAvailable =
+  do qRandom <- splitRNG
+     return ResourceQ { qDiscarded = [], .. }
+
+-- | Take something from the deck.  If the deck is empty,
+-- shuffle the discard and use it as the new deck.
+-- Returns 'Nothing' if both the deck and the discard are empty.
+rqTake :: ResourceQ a -> Maybe (a, ResourceQ a)
+rqTake ResourceQ { .. } =
+  case qAvailable of
+    a : as -> Just (a, ResourceQ { qAvailable = as, .. })
+    [] -> case runRNG qRandom (shuffle qDiscarded) of
+            (a:as,g) -> Just (a, ResourceQ { qAvailable = as
+                                           , qDiscarded = []
+                                           , qRandom    = g })
+            ([],_) -> Nothing
+
+-- | Place something in the discard pile.
+rqDiscard :: a -> ResourceQ a -> ResourceQ a
+rqDiscard a ResourceQ { .. } = ResourceQ { qDiscarded = a : qDiscarded, .. }
+
+
+
